6b4321e854794f3e1b2e27c1a196fb3aef5f9753
Saravia01 <147068606+Saravia01@users.noreply.github.com>
2023-10-15 23:44:43 -0600

    V2

--- angularPractica/README.md
+++ angularPractica/README.md
@@ -1,4 +1,4 @@
-# AngularPractica
+# AngularProyecto
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 16.2.4.
 
--- angularPractica/src/app/app.component.spec.ts
+++ angularPractica/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angularPractica'`, () => {
+  it(`should have as title 'angularProyecto'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angularPractica');
+    expect(app.title).toEqual('angularProyecto');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('angularPractica app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('angularProyecto app is running!');
   });
 });
